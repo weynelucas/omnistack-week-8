bc12e85f86fc2e6a84d362aeab679710de25b4a0
Lucas Weyne <weynelucas@gmail.com>
2019-08-09 15:44:30 -0300

    Main page structure

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,8 +1,32 @@
 import React from 'react';
 
+import './Main.css';
+import logo from '../assets/logo.svg';
+import like from '../assets/like.svg';
+import dislike from '../assets/dislike.svg';
+
 
 export default function Main({ match }) {
   return (
-    <h1>{match.params.id}</h1>
+    <div className="Main">
+      <img src={logo} alt="Tindev"/>
+      <ul>
+        <li>
+          <img src="https://avatars2.githubusercontent.com/u/6399202?v=4" alt="Francisco Thiago"/>
+          <footer>
+            <strong>Francisco Thiago</strong>
+            <p>Web Developer. Currently working with Vue.js and Laravel.</p>
+          </footer>
+          <div className="Main-buttons">
+            <button>
+              <img src={like} alt="Like"/>
+            </button>
+            <button>
+              <img src={dislike} alt="Dislike"/>
+            </button>
+          </div>
+        </li>
+      </ul>
+    </div>
   );
 }
