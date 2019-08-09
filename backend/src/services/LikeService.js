171859489d55f0653f2605c80bfa2de05cf160b6
Lucas Weyne <weynelucas@gmail.com>
2019-08-09 12:08:05 -0300

    Fix like service

--- backend/src/services/LikeService.js
+++ backend/src/services/LikeService.js
@@ -5,14 +5,14 @@ const Dev = model('Dev');
 
 module.exports = {
   async like(source, target, dislike=false) {
-    const property = dislike ? 'dislike' : 'like';
+    const property = dislike ? 'dislikes' : 'likes';
 
-    if (!source[property].includes(source.id)) {
+    if (!source[property].includes(target._id)) {
       source[property].push(target.id);
       await source.save();
     }
 
-    const match = property === 'like' && target.likes.includes(source.id);
+    const match = !dislike && target.likes.includes(source.id);
 
     return { dev: source, match };
   }
