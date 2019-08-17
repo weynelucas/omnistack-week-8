0784f14557bad2186c550c7f9d3cb38880a6412b
Lucas Weyne <weynelucas@gmail.com>
2019-08-17 16:24:34 -0300

    Create app

---
+++ mobile/src/index.js
@@ -0,0 +1,17 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+
+import Login from './pages/Login';
+
+export default function App() {
+  return (
+    <Login />
+  );
+};
