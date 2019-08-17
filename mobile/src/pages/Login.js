9ea893db55074b607eb88a680aa891e16bf0eee8
Lucas Weyne <weynelucas@gmail.com>
2019-08-17 16:49:13 -0300

    Login style

--- mobile/src/pages/Login.js
+++ mobile/src/pages/Login.js
@@ -1,8 +1,73 @@
 import React from 'react';
-import {Text} from 'react-native';
+import { 
+  Platform,
+  KeyboardAvoidingView, 
+  Text,
+  StyleSheet, 
+  Image, 
+  TextInput, 
+  TouchableOpacity 
+} from 'react-native';
+
+import logo from '../assets/logo.png';
 
 export default function Login() {
   return (
-    <Text>Login</Text>
+    <KeyboardAvoidingView 
+      behavior="padding"
+      enabled={Platform.OS === 'ios'}
+      style={styles.container}
+    >
+      <Image source={logo} />
+
+      <TextInput  
+        autoCapitalize="none"
+        autoCorrect={false}
+        placeholder="Entre com seu usuário do Github"
+        placeholderTextColor="#999"
+        style={styles.input}
+      />
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Enviar</Text>
+      </TouchableOpacity>
+    </KeyboardAvoidingView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5F5F5',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 30
+  },
+
+  input: {
+    height: 46,
+    alignSelf: 'stretch',
+    backgroundColor: '#FFF',
+    borderWidth: 1,
+    borderColor: '#DDD',
+    borderRadius: 4,
+    marginTop: 20,
+    paddingHorizontal: 15
+  },
+
+  button: {
+    height: 46,
+    alignSelf: 'stretch',
+    backgroundColor: '#DF4723',
+    borderRadius: 4,
+    marginTop: 10,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  buttonText: {
+    color: '#FFF',
+    fontWeight: 'bold',
+    fontSize: 16
+  }
+})
